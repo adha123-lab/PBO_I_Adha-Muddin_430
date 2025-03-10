4bac425874b87db68cb1171809f9f5371d4059c8
adha123-lab <madhamuddin@gmail.com>
2025-03-10 10:58:36 +0700

    adha

---
+++ Tugas 1 PBO/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        public static scanner Input = new scenner (system.in)
+
+         System.out.print("login sebagai")
+         System.out.print("login Admin")
+         System.out.print("login Mahasiswa")
+         System.out.print("masukan pilihan")
+         System.out.("s")
+            choice 1=
+            user = ("Admin")
+            Password = ("Password")
+            user = ("Abdul Mukmin")
+            NIM =("472")
+
+
+
+
+
+
+
